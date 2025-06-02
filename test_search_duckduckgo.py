06bde9fcd780336516aa97483f4d25df95ad65cc
Zhanna Ovcharenko <ZOvcharenko99@gmail.com>
2025-06-03 00:02:20 +0300

    обновила тест

--- test_search_duckduckgo.py
+++ test_search_duckduckgo.py
@@ -1,15 +1,13 @@
-from selene import browser, have, be
+from selene import browser, be, have
 
 
-# Positive test
-def test_duckduckgo_search_positive():
+def test_duckduckgo_search_positive(browser_settings):
     browser.open('https://duckduckgo.com')
     browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('qa.guru').press_enter()
-    browser.element('div h2 a span').should(have.text('Курсы тестировщиков – обучение тестированию онлайн'))
+    browser.element('div h2 a span').should(have.text('Курсы тестировщиков - обучение тестированию онлайн'))
 
 
-# Negative test
-def test_duckduckgo_search_negative():
+def test_duckduckgo_search_negative(browser_settings):
     browser.open('https://duckduckgo.com')
-    browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('khpfsrmnhgdde').press_enter()
-    browser.element('div h2 a span').should(have.text('По запросу khpfsrmnhgdde ничего не найдено.'))
+    browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('khpfsnmnhgdde').press_enter()
+    browser.element('div h2 a span').should(have.text('По запросу khpfsnmnhgdde ничего не найдено.'))
