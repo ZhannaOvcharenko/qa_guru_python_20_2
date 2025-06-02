b733d838619e528f4c6e3815158a7ad27396b1c4
Zhanna Ovcharenko <ZOvcharenko99@gmail.com>
2025-06-02 23:09:43 +0300

    Rename def for test_search_duckduckgo and delete blank lines

--- test_search_duckduckgo.py
+++ test_search_duckduckgo.py
@@ -1,13 +1,15 @@
 from selene import browser, have, be
 
-# Positive test
-def test_duckduckgo_search():
-    browser.open('https://duckduckgo.com')
-    browser.element("[placeholder='Поиск без отслеживания']").should(be.blank).type('qa.guru').press_enter()
-    browser.element('div h2 a span').should(have.text('Курсы тестировщиков — обучение тестированию онлайн'))
 
-#Negative test
-def test_duckduckgo_search():
+# Positive test
+def test_duckduckgo_search_positive():
     browser.open('https://duckduckgo.com')
-    browser.element("[placeholder='Поиск без отслеживания']").should(be.blank).type('khpfsrmnhgdde').press_enter()
+    browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('qa.guru').press_enter()
+    browser.element('div h2 a span').should(have.text('Курсы тестировщиков – обучение тестированию онлайн'))
+
+
+# Negative test
+def test_duckduckgo_search_negative():
+    browser.open('https://duckduckgo.com')
+    browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('khpfsrmnhgdde').press_enter()
     browser.element('div h2 a span').should(have.text('По запросу khpfsrmnhgdde ничего не найдено.'))
