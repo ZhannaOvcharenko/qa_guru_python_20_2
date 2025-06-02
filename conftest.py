e8c84a7f34f641b208de406bd53819eb99506d75
Zhanna Ovcharenko <ZOvcharenko99@gmail.com>
2025-06-03 00:27:58 +0300

    обновила conftest и requirements.txt

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selene import browser
 
 @pytest.fixture(scope="session")
 def browser_settings():
-    browser.config.window_width = 1366
+    browser.config.window_width = 1380
     browser.config.window_height = 607
     yield
     browser.quit()
