07737182a0b630f5201a256b7d6033203fead2ff
Zhanna Ovcharenko <ZOvcharenko99@gmail.com>
2025-06-02 22:52:08 +0300

    Обновила test_search_duckduckgo.py

--- test_search_duckduckgo.py
+++ test_search_duckduckgo.py
@@ -1,6 +1,13 @@
 from selene import browser, have, be
 
+# Positive test
 def test_duckduckgo_search():
     browser.open('https://duckduckgo.com')
     browser.element("[placeholder='Поиск без отслеживания']").should(be.blank).type('qa.guru').press_enter()
     browser.element('div h2 a span').should(have.text('Курсы тестировщиков — обучение тестированию онлайн'))
+
+#Negative test
+def test_duckduckgo_search():
+    browser.open('https://duckduckgo.com')
+    browser.element("[placeholder='Поиск без отслеживания']").should(be.blank).type('khpfsrmnhgdde').press_enter()
+    browser.element('div h2 a span').should(have.text('По запросу khpfsrmnhgdde ничего не найдено.'))
