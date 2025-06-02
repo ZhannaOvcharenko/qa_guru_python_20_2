0d2efe46a56ba46783b97c51af424e6ce1f66130
Zhanna Ovcharenko <ZOvcharenko99@gmail.com>
2025-06-02 22:29:02 +0300

    Обновила conftest.py

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selene import browser
+
+@pytest.fixture(scope="session")
+def browser_settings():
+    browser.config.window_width = 1366
+    browser.config.window_height = 607
+    yield
+    browser.quit()
